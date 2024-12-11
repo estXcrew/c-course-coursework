a0b85c8e1561ac5ca57b75d8500a4dc1763aa49e
Raul Semsugov <raul.semsugov@tuni.fi>
2024-12-12 01:14:17 +0200

    gitignore, type lims

---
+++ 1/type_lims/arvorajat.c
@@ -0,0 +1,86 @@
+#include <string.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+signed char, unsigned char, short int, unsigned short int ja int
+ */
+
+int main(int argc, char** argv)
+{
+    int printcomma;
+    int i;
+    int res;
+
+    for(i=1; i<argc; i++)
+    {
+        printcomma = 0;
+
+        res = atoi(argv[i]);
+        printf("%d: ", res);
+
+
+        if(res < SCHAR_MAX && res > SCHAR_MIN)
+        {
+            if(printcomma){
+                printf(", ");
+            } else {
+                printcomma = 1;
+            }
+
+            printf("signed char");
+        }
+
+        if(res < UCHAR_MAX && res >= 0)
+        {
+            if(printcomma){
+                printf(", ");
+            } else {
+                printcomma = 1;
+            }
+
+            printf("unsigned char");
+        }
+
+
+        if(res < SHRT_MAX && res > SHRT_MIN)
+        {
+            if(printcomma){
+                printf(", ");
+            } else {
+                printcomma = 1;
+            }
+
+            printf("short int");
+        }
+
+        if(res < USHRT_MAX && res >= 0)
+        {
+            if(printcomma){
+                printf(", ");
+            } else {
+                printcomma = 1;
+            }
+
+            printf("unsigned short int");
+        }
+
+        if(res < INT_MAX && res > INT_MIN)
+        {
+            if(printcomma){
+                printf(", ");
+            } else {
+                printcomma = 1;
+            }
+
+            printf("int");
+        }
+
+
+        if(i+0 != argc){ 
+            printf("\n");
+        }
+    }
+    return 0;
+}
