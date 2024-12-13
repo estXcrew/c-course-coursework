d8475f04d9436ebc3ee249e85eb2b4344508eee5
Raul Semsugov <raul.semsugov@tuni.fi>
2024-12-13 18:29:10 +0200

    almost working unsigned ex (doesnt print last val)

---
+++ 1/unsigned/unsigned.c
@@ -0,0 +1,97 @@
+#include <string.h>
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "unsigned.h"
+
+/*Eli ensin tulostetaan arvo -sade = MAX - sade + 1, sitten -sade + askel = MAX - sade + 1 + askel,
+     sitten -sade + 2*askel = MAX - sade + 1 + 2*askel, ja niin edelleen, kunnes intervallin loppuraja sade ylitetään. */
+
+void ucharIntervalli(unsigned char sade, unsigned char askel){
+    unsigned char value;
+
+    value = -sade;
+
+    /*printf("started on %lu (MAX = %lu)", value, UCHAR_MAX);*/
+
+    printf("unsigned char: ");
+
+    while(value != sade)
+    {
+        printf("%u ", value);
+                value += askel;
+    }
+    /*printf("broke on %lu", value);*/
+    printf("\n");
+}
+
+
+void ushortIntervalli(unsigned short sade, unsigned short askel){
+    unsigned short value;
+
+    value = -sade;
+
+    /*printf("started on %lu (MAX = %lu)", value, UCHAR_MAX);*/
+
+    printf("unsigned short: ");
+
+    while(value != sade)
+    {
+        printf("%u ", value);
+                value += askel;
+    }
+    /*printf("broke on %lu", value);*/
+    printf("\n");
+}
+
+void uintIntervalli(unsigned int sade, unsigned int askel){
+    unsigned int value;
+
+    value = -sade;
+
+    /*printf("started on %lu (MAX = %lu)", value, UCHAR_MAX);*/
+
+    printf("unsigned int: ");
+
+    while(value != sade)
+    {
+        printf("%u ", value);
+                value += askel;
+    }
+    /*printf("broke on %lu", value);*/
+    printf("\n");
+}
+
+void ulongIntervalli(unsigned long sade, unsigned long askel){
+    unsigned long value;
+
+    value = -sade;
+
+    /*printf("started on %lu (MAX = %lu)", value, UCHAR_MAX);*/
+
+    printf("unsigned long: ");
+
+    while(value != sade)
+    {
+        printf("%lu ", value);
+                value += askel;
+    }
+    /*printf("broke on %lu", value);*/
+    printf("\n");
+}
+
+
+int main(){
+
+    ucharIntervalli(150, 50);
+    ushortIntervalli(45000, 15000);
+    uintIntervalli(3300000000, 1100000000);
+    ulongIntervalli(9000000000000000000, 3000000000000000000);
+/*    ucharIntervalli(40, 75);
+    ushortIntervalli(65535, 65535); 
+    uintIntervalli(400000, 750000);
+    ulongIntervalli(400000000, 1500000000);*/
+
+    return 0;
+}
---
+++ 1/unsigned/unsigned.h
@@ -0,0 +1,13 @@
+#ifndef __UNSIGNED_H__
+#define __UNSIGNED_H__
+
+void ucharIntervalli(unsigned char sade, unsigned char askel);
+/*Intervallin arvojen läpikäynnissä / tulostuksessa käytetään unsigned char -tyyppiä.*/
+void ushortIntervalli(unsigned short sade, unsigned short askel);
+/*Intervallin arvojen läpikäynnissä / tulostuksessa käytetään unsigned short -tyyppiä.*/
+void uintIntervalli(unsigned int sade, unsigned int askel);
+/*Intervallin arvojen läpikäynnissä / tulostuksessa käytetään unsigned int -tyyppiä.*/
+void ulongIntervalli(unsigned long sade, unsigned long askel);
+/*Intervallin arvojen läpikäynnissä / tulostuksessa käytetään unsigned long -tyyppiä.*/
+
+#endif
