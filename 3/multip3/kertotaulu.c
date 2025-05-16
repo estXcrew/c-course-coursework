27a159bc5c8585216b079893fbda8ee737411af4
Raul Semsugov <raul.semsugov@tuni.fi>
2025-05-16 15:36:20 +0300

    add multip e x

--- 3/multip3/kertotaulu.c
+++ 3/multip3/kertotaulu.c
@@ -7,6 +7,33 @@
 
 int main () {
 
+    Kertotaulu* k;
+    int i, j;
+    int dx, dy;
+
+    const uint s_x = 2;
+    const uint e_x = 9;
+    const uint s_y = 11;
+    const uint e_y = 17;
+
+    k = luoKertotaulu(s_x, e_x, s_y, e_y);
+
+    dx = e_x-s_x;
+    dy = e_y-s_y;
+
+    printf("printing arr vals from main loop (dx = %d, dy = %d)..\n", dx, dy);
+
+    for(i=0; i<dx; i++)
+    {
+        for(j=0; j<dy;j++)
+        {
+            printf("%12u", k->kertotaulu[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
+    
+    return 0;
 }
 
 #endif
@@ -14,22 +41,20 @@ int main () {
 Kertotaulu* luoKertotaulu(uint a, uint b, uint c, uint d)
 {
     Kertotaulu* krt;
-    uint** table;
 
-    size_t dim_x;
-    size_t dim_y;
-
-    size_t tbl_size;
+    size_t dim_rows;
+    size_t dim_cols;
+    uint temp_val;
 
     unsigned int i;
     unsigned int j;
+
 
     if(a > b || c > d)
     {
         return NULL;
     }
 
-    /* allocate multiplication table struct */
     krt = (Kertotaulu*) malloc(sizeof(Kertotaulu));
     krt->a = a;
     krt->b = b;
@@ -37,34 +62,39 @@ Kertotaulu* luoKertotaulu(uint a, uint b, uint c, uint d)
     krt->d = d;
 
     /* allocate table itself */
-    dim_x = (krt->b) - (krt->a) + 1; /*# rows*/
-    dim_y = (krt->d) - (krt->c) + 1; /*# cols*/
+    dim_cols = (krt->b) - (krt->a) + 2; 
+    dim_rows = (krt->d) - (krt->c) + 2; 
+
     /* allocate row pointers */
-    krt->kertotaulu = (uint**)malloc(sizeof(uint)*dim_x);
+    /*[row][col]*/
+    krt->kertotaulu = (uint**)malloc(sizeof(uint**)*dim_rows);
     /* allocate short 1d column memory for each row*/
-    for(int i=0; i<dim_x; i++)
+    for(i=0; i<dim_cols; i++)
     {
-        krt->kertotaulu[i] = (uint*)malloc(sizeof(uint)*dim_y);
+        krt->kertotaulu[i] = (uint*)malloc(sizeof(uint*)*dim_cols);
     }
 
     /* now we can access the regular 2d row-major array: kertotaulu[row][col]*/
-    krt->kertotaulu[0] = 0;
-    /* row 0, col a+1 - b*/
-    for(i=0; i<(b-1); i++)
+    krt->kertotaulu[0][0] = 0;
+    /* first row, cols 1-d*/
+    for(i=0; i<(dim_rows-1); i++)
     {
-        krt->kertotaulu[i+1] = i+a;
+        krt->kertotaulu[0][i+1] = i+a;
     }
-
     /* row n+1 - m, col 0*/
-    for(i=0; i<(d-1); i++)
+    for(i=0; i<(dim_cols-1); i++)
     {
         krt->kertotaulu[i+1][0] = i+c;
     }
 
     /* calculate values*/
-    for(i=1; i<dim_x; i++)
+    for(i=1; i<dim_rows; i++)
     {
-        for(j=1; j<dim_y; )
+        for(j=1; j<dim_cols;j++)
+        {
+            temp_val = krt->kertotaulu[0][j] * krt->kertotaulu[i][0];
+            krt->kertotaulu[i][j] = temp_val;
+        }
     }
 
 
